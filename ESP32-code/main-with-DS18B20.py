4cbeb236579632c425827354def0ec93901194bc
Duncan <49673638+loddington@users.noreply.github.com>
2024-04-08 22:09:02 +1000

    Update main-with-DS18B20.py

--- ESP32-code/main-with-DS18B20.py
+++ ESP32-code/main-with-DS18B20.py
@@ -3,23 +3,27 @@ import network
 import time
 import urequests
 import esp32
+from machine import Pin
 from onewire import OneWire
 from ds18x20 import DS18X20
 
-machine.freq(80000000)
+# Set sleep duration (milliseconds)
+sleep_duration = 60000  # 60 seconds = 60000 milliseconds
 
 # Define GPIO pin for the button
 BUTTON_PIN = 27
+# Define GPIO pin for the DS18B20 temperature sensor
+TEMP_SENSOR_PIN = 19
 
 # Define the WiFi credentials
-WIFI_SSID = "JibbenbarSSID"
-WIFI_PASSWORD = "Your-WiFi-PW"
+WIFI_SSID = "Your SSID"
+WIFI_PASSWORD = "YOUR SSID PW"
 
 # Define the server IP of the Data Logger API and endpoint
 SERVER_IP = "severn-data.loddington.com"
 SERVER_PORT = 5000
-TEMPERATURE_ENDPOINT = "/sensors/temperature/increment"
-BUTTON_ENDPOINT = "/sensors/button/press"
+ENDPOINT_BUCKET = "sensors/bucket_tips/increment"
+ENDPOINT_TEMPERATURE = "sensors/sun_temp"
 
 # Define the delay between connection attempts
 RETRY_DELAY = 5
@@ -35,95 +39,92 @@ def connect_wifi():
             time.sleep(1)
     print("WiFi Connected:", wlan.ifconfig())
 
-# Function to make the HTTP request to the Data Logger for button press
-def send_button_press():
+# Function to make the HTTP request to the Data Logger
+def make_request_bucket():
     try:
-        response = urequests.put("http://{}:{}/{}".format(SERVER_IP, SERVER_PORT, BUTTON_ENDPOINT))
+        response = urequests.put("http://{}:{}/{}".format(SERVER_IP, SERVER_PORT, ENDPOINT_BUCKET))
         if response.status_code == 200:
-            print("Button press sent successfully")
+            print("Requests Successful")
+            response.close()
+            return True
         else:
-            print("Failed to send button press")
-        response.close()
+            print("Request(s) Failed:")
+            if response.status_code != 200:
+                print("  - Request to SERVER_IP failed with status code:", response.status_code)
+                # Retry the connection to SERVER_IP after a delay
+                print("Retrying connection to SERVER_IP in 5 seconds...")
+                time.sleep(5)
+                response = urequests.put("http://{}:{}/{}".format(SERVER_IP, SERVER_PORT, ENDPOINT_BUCKET))
+                if response.status_code == 200:
+                    print("Retried connection to SERVER_IP successful")
+                else:
+                    print("Retry to SERVER_IP failed, going back to sleep")
+                    return False
     except OSError as e:
-        print("Exception occurred during button press request:", e)
-
-# Function to make the HTTP request to the Data Logger for temperature
-def send_temperature(temperature):
-    try:
-        response = urequests.put("http://{}:{}/{}".format(SERVER_IP, SERVER_PORT, TEMPERATURE_ENDPOINT), json={"temperature": temperature})
-        if response.status_code == 200:
-            print("Temperature sent successfully")
+        if e.errno == 113:
+            print("Connection aborted. Data logger may be unreachable.")
         else:
-            print("Failed to send temperature")
-        response.close()
+            print("Exception occurred during request:", e)
+        return False
+
+def make_request_temperature(temperature):
+    try:
+        data = {'sensor_value': temperature}
+        headers = {'Content-Type': 'application/json'}
+        response = urequests.put("http://{}:{}/{}".format(SERVER_IP, SERVER_PORT, ENDPOINT_TEMPERATURE), headers=headers, json=data)
+        if response.status_code == 200:
+            print("Temperature Request Successful")
+            response.close()
+            return True
+        else:
+            print("Temperature Request Failed with status code:", response.status_code)
+            return False
     except OSError as e:
         print("Exception occurred during temperature request:", e)
+        return False
+
+
+
+# Function to read temperature from DS18B20 sensor
+def read_temperature():
+    dat = Pin(TEMP_SENSOR_PIN)
+    ds_sensor = DS18X20(OneWire(dat))
+    roms = ds_sensor.scan()
+    ds_sensor.convert_temp()
+    time.sleep_ms(750)  # Wait for conversion to finish (750ms for DS18B20)
+    temperature = ds_sensor.read_temp(roms[0])  # Assuming only one sensor connected
+    return temperature
 
 # Main loop
 def main():
     # Disable unused peripherals and pins to reduce power consumption
-    machine.Pin(2, machine.Pin.IN)  # GPIO2 is disabled
-    #machine.Pin(4, machine.Pin.IN)  # GPIO4 is disabled
-    machine.Pin(5, machine.Pin.IN)  # GPIO5 is disabled
-    machine.Pin(12, machine.Pin.IN) # GPIO12 is disabled
-    machine.Pin(13, machine.Pin.IN) # GPIO13 is disabled
-    machine.Pin(14, machine.Pin.IN) # GPIO14 is disabled
-    machine.Pin(15, machine.Pin.IN) # GPIO15 is disabled
-    machine.Pin(25, machine.Pin.IN) # GPIO25 is disabled
-    machine.Pin(26, machine.Pin.IN) # GPIO26 is disabled
-    #machine.Pin(27, machine.Pin.IN) # GPIO27 is disabled
-    machine.Pin(32, machine.Pin.IN) # GPIO32 is disabled
-    machine.Pin(33, machine.Pin.IN) # GPIO33 is disabled
+    machine.Pin(4, machine.Pin.IN)  # GPIO4 is disabled
+    # machine.lightsleep()  # Start in light sleep mode
     print('hi')
-  
-    # Initialize DS18X20 sensor
-    ow = OneWire(machine.Pin(4))  # Pin where DS18D20 sensor is connected
-    ds = DS18X20(ow)
+
+    # Configure button pin with pull-up
+    button = machine.Pin(BUTTON_PIN, machine.Pin.IN, machine.Pin.PULL_UP)
 
     while True:
-        # Configure button pin with pull-up and wakeup capability
-        button = machine.Pin(BUTTON_PIN, machine.Pin.IN, machine.Pin.PULL_UP)
-        esp32.wake_on_ext0(pin=button, level=esp32.WAKEUP_ALL_LOW)
+        temperature = read_temperature()  # Read temperature
+        print("DS18B20 Temperature reading is:", temperature)  # Print temperature reading
+        make_request_temperature(temperature)  # Send temperature reading to server
+        
+        time.sleep(0.5) #This is just for debugging. 
+        machine.lightsleep(sleep_duration)  # Sleep 
 
-        # Enter light sleep
-        print("Entering light sleep...")
-        time.sleep_ms(1500)
-        machine.lightsleep()
-
-        # Code executed after waking from light sleep (button press)
-        print("Woke from light sleep! Button pressed.")
-        button = machine.Pin(BUTTON_PIN, machine.Pin.IN, machine.Pin.PULL_UP)
-        button_pressed_time = 0
-
-        while True:
-            if button.value() == 0:  # Button is pressed
-                button_pressed_time += 1
-            else:  # Button released
-                if 100 <= button_pressed_time < 1000:
-                    time.sleep_ms(500)  # Debounce the button
-                    connect_wifi()
-                    send_button_press()
-                    time.sleep_ms(200)
-                    network.WLAN(network.STA_IF).active(False)  # Disable WiFi
-                    print("WiFi Disabled going back to sleep")
-                    time.sleep_ms(500)
-                    machine.lightsleep()
-                button_pressed_time = 0
-
-            # Check temperature every 15 minutes
-            if time.time() % 900 == 0:  # 900 seconds = 15 minutes
-                ds.convert_temp()
-                time.sleep_ms(750)
-                temperature = ds.read_temp()
-                if temperature is not None:
-                    print("Temperature:", temperature)
-                    connect_wifi()
-                    send_temperature(temperature)
-                    time.sleep_ms(200)
-                    network.WLAN(network.STA_IF).active(False)  # Disable WiFi
-                    print("WiFi Disabled going back to sleep")
-                    time.sleep_ms(500)
-                    machine.lightsleep()
+        # Check for button press after waking up from light sleep
+        if button.value() == 0:  # Button is pressed
+            print("Button pressed!")
+            # Your button handling code here (e.g., connect to WiFi, send data)
+            connect_wifi()
+            if make_request_bucket():
+                time.sleep_ms(200)
+                network.WLAN(network.STA_IF).active(False)  # Disable WiFi
+                print("WiFi Disabled going back to sleep")
+            else:
+                print("Retrying in {} seconds...".format(RETRY_DELAY))
+                time.sleep(RETRY_DELAY)
 
 if __name__ == "__main__":
     main()
