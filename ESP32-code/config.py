39870434fc27e5d55ec55df9bae3639a891345df
Duncan <49673638+loddington@users.noreply.github.com>
2024-10-27 21:23:34 +1100

    Create config.py

---
+++ ESP32-code/config.py
@@ -0,0 +1,9 @@
+# WiFi credentials
+WIFI_SSID = "XXXXXXX"
+WIFI_PASSWORD = "XXXXXXXXXXXXXX"
+
+# Server URL for the PUT request
+SERVER_URL = "http://192.168.123.45:5000/sensors/bucket_tips/increment"
+
+# Hostname for DHCP identification
+HOSTNAME = "ESP_Jibbenbar_Rain"
